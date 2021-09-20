c7110c5aba2816b50d8a250fd9d64320f606a64b
shuharib0t <japaishikawa@gmail.com>
2021-09-20 11:41:34 -0300

    Seller entity class

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -2,7 +2,7 @@ package model.entities;
 
 import java.io.Serializable;
 
-public class Department implements Serializable{
+public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
